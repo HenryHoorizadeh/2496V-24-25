170e5d2f519a0c144ac80c200a94a112060342b3
zhihuid4094 <athenad0604@gmail.com>
2024-08-22 19:31:31 -0700

    speed change again

--- src/main.cpp
+++ src/main.cpp
@@ -275,16 +275,14 @@ void opcontrol() {
     if (((con.get_digital(E_CONTROLLER_DIGITAL_R1) && NEWR2) || (NEWR1 && con.get_digital(E_CONTROLLER_DIGITAL_R2))) || ((NEWR1 && NEWR2) || (con.get_digital(E_CONTROLLER_DIGITAL_R1) && con.get_digital(E_CONTROLLER_DIGITAL_R2)))){
       //Double Press action
       INTAKE.move(127);
-      HOOKS.move_velocity(-650);
+      HOOKS.move_velocity(-450);
     } else if  (con.get_digital(E_CONTROLLER_DIGITAL_R1)) {
 			INTAKE.move(-127);
       HOOKS.move(-127);
-		}
-		else if (con.get_digital(E_CONTROLLER_DIGITAL_R2)) {
+		} else if (con.get_digital(E_CONTROLLER_DIGITAL_R2)) {
 			INTAKE.move(127);
       HOOKS.move(127);
-		}
-		else {
+		} else {
 			INTAKE.move(0);
       HOOKS.move(0);
 		}
